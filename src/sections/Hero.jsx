9b295a8683a46fa5d92c362623bc481c145c8efa
Karthikeyan1701 <karthikvijay171@gmail.com>
2026-01-03 21:49:49 +0530

    Added some background styles in Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,25 @@
 export const Hero = () => {
-    return <section></section>;
-}
+  return (
+    <section className="relative min-h-screen flex items-center overflow-hidden">
+      {/* Background */}
+      <div className="absolute inset-0">
+        <img
+          src="/hero-bg.jpg"
+          alt="Hero image"
+          className="w-full h-full object-cover opacity-40"
+        />
+        <div className="absolute inset-0 bg-linear-to-b from-background/20 via-background/80 to-background"></div>
+      </div>
+
+      {/* Green Dots */}
+      <div>
+        {[...Array(30)].map((_, i) => (
+          <div
+            key={i}
+            className="absolute w-1.5 h-1.5 rounded-full opacity-60"
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
